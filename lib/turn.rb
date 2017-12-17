b2e4835ba0c5938ee9f66dcd507b18a7b2b55c7d
Helen Liutongco <bellpickle@gmail.com>
2017-12-17 03:12:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,22 @@ def within_board?(index)
   index.between?(0,8)
 end
 
-# code your #valid_move? method here
 def valid_move?(board,index)
   (within_board?(index) && !position_taken?(board,index)) ? true : false
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(position)
+  position.to_i - 1
+end
+
+def move(board,index,character = "X")
+  board[index] = character
 end
